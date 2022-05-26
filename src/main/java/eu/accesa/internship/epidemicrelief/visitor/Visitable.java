c327744aa59e311f01ab9eb0ac41f169a4ecb69f
Robert Spac <robert.spac@accesa.eu>
2022-05-26 19:51:45 +0300

    Task 5 added documentation for Visitor/Visitable/HouseholdMember

--- src/main/java/eu/accesa/internship/epidemicrelief/visitor/Visitable.java
+++ src/main/java/eu/accesa/internship/epidemicrelief/visitor/Visitable.java
@@ -4,6 +4,15 @@ import eu.accesa.internship.epidemicrelief.visitor.model.ProductNecessity;
 
 import java.util.List;
 
+/**
+ * Contains business logic related to Visitable.
+ */
 public interface Visitable {
+    /**
+     * Get a list of ProductNecessity specific for a Visitor
+     *
+     * @param visitor a {@link Visitor}
+     * @return a list of {@link ProductNecessity}
+     */
     List<ProductNecessity> productNecessityList(Visitor visitor);
 }
--- src/main/java/eu/accesa/internship/epidemicrelief/visitor/model/HouseholdMember.java
+++ src/main/java/eu/accesa/internship/epidemicrelief/visitor/model/HouseholdMember.java
@@ -2,6 +2,9 @@ package eu.accesa.internship.epidemicrelief.visitor.model;
 
 import eu.accesa.internship.epidemicrelief.visitor.Visitable;
 
+/**
+ * Contains business logic related to HouseholdMember.
+ */
 public interface HouseholdMember extends Visitable {
 
 }
